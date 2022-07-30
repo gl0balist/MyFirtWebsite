306c401db2e6bab35da179e79a0df275f895b353
Andrei <drong33@gmail.com>
2022-07-30 23:27:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 tl.fromTo("h1", { opacity: 0 }, { opacity: 1, duration: 2 });
 tl.fromTo("h2", { opacity: 0 }, { opacity: 1, duration: 3 }, "-=1");
 
-gsap.to('.top-cloud', {x: -600, duration: 2});
-gsap.to('.bottom-cloud', {x: 600, duration: 2});
+gsap.to('.top-cloud', {x: -75, duration: 2});
+gsap.to('.bottom-cloud', {x: 205, duration: 2});
